26fe7c35e00173a1c925f459d729e6c8ff36f35d
Pritom2357 <pritombiswas9999@gmail.com>
2025-04-11 15:29:56 +0600

    fixed issues with add button

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -310,7 +310,7 @@ function Home() {
                         to="/create-resource" 
                         className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 text-sm font-medium"
                       >
-                        Add Resource
+                        Add...
                       </Link>
                     </div>
                   </div>
